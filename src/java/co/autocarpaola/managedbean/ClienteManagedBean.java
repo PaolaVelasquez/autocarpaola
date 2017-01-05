25473eaabdb38b7ddfa40b6d011e8de76ab59ea9
yo <yo@DESKTOP-MR3L7R2>
2017-01-04 20:13:26 -0500

    co.autocarpaola.entity

---
+++ src/java/co/autocarpaola/managedbean/ClienteManagedBean.java
@@ -0,0 +1,108 @@
+package co.autocarpaola.managedbean;
+
+import co.autocarpaola.entity.Cliente;
+import co.autocarpaola.sessionbeans.ClienteFacadeLocal;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
+
+
+@Named(value = "clienteManagedBean")
+@RequestScoped
+public class ClienteManagedBean implements Serializable {
+    @EJB
+    private ClienteFacadeLocal ClienteEJB;
+    
+    private Cliente cliente;
+    
+    private String accion;
+    
+    public ClienteManagedBean() {
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getAccion() {
+        return accion;
+    }
+
+    public void setAccion(String accion) {
+        this.accion = accion;
+    }
+
+    @PostConstruct
+    public void init() {
+       cliente = new Cliente();
+    }
+    
+    public void  registroCliente (){
+        try{
+            ClienteEJB.create(cliente);
+            manejarExito ("Registrar");
+        }catch (Exception e){
+            manejarError (e);
+        }
+    }
+    
+    public String registrar(){
+        return "listar";
+    }
+    
+    public List<Cliente> getClie(){
+        try{
+            return this.ClienteEJB.findAll();
+        }catch (Exception e) {
+            manejarError(e);
+        }
+        return null;
+    }
+     public void elimarCliente (Cliente cli){
+        try {
+            ClienteEJB.remove(cli);
+            manejarExito("Eliminado");
+        }catch (Exception e){
+            manejarError (e);
+        }
+    }
+    public void leer(Cliente leerCliente){
+        cliente = leerCliente;
+        this.setAccion("M");
+    }
+    public void modificar (){
+        
+          try {
+             ClienteEJB.edit(cliente);
+            manejarExito("Editado");
+        }catch (Exception e){
+            manejarError (e);
+        }
+    }
+    
+    
+    
+    private void manejarExito(String operacion) {
+        String msg = "Se ha realizado exitosamente la operacion de " + operacion;
+        FacesContext context =FacesContext.getCurrentInstance();
+        context.addMessage(null,new FacesMessage(msg));
+        FacesMessage sal = new FacesMessage(FacesMessage.SEVERITY_INFO,"Operación exitosa: ", msg);
+        RequestContext.getCurrentInstance().showMessageInDialog(sal);
+    }
+    private void manejarError(Exception e) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Se produjo el sigiente error: ", e.getMessage()));
+        FacesMessage msg =new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error al insertar: ", e.getMessage());
+        RequestContext.getCurrentInstance().showMessageInDialog(msg);
+    }
+}
---
+++ src/java/co/autocarpaola/managedbean/ConsesionarioManagedBean.java
@@ -0,0 +1,108 @@
+package co.autocarpaola.managedbean;
+
+import co.autocarpaola.entity.Concesionario;
+import co.autocarpaola.sessionbeans.ConcesionarioFacadeLocal;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
+
+
+@Named(value = "consesionarioManagedBean")
+@RequestScoped
+public class ConsesionarioManagedBean implements Serializable {
+    @EJB
+    private ConcesionarioFacadeLocal ConcesionarioEJB;
+    
+    private Concesionario concesionario;
+    
+    private String accion;
+    
+    public ConsesionarioManagedBean() {
+    }
+
+    public Concesionario getConcesionario() {
+        return concesionario;
+    }
+
+    public void setConcesionario(Concesionario concesionario) {
+        this.concesionario = concesionario;
+    }
+    
+    public String getAccion() {
+        return accion;
+    }
+
+    public void setAccion(String accion) {
+        this.accion = accion;
+    }
+
+    @PostConstruct
+    public void init() {
+       concesionario = new Concesionario();
+    }
+    
+    public void  registroConcesioanrio (){
+        try{
+            ConcesionarioEJB.create(concesionario);
+            manejarExito ("Registrar");
+        }catch (Exception e){
+            manejarError (e);
+        }
+    }
+    
+    public String registrar(){
+        return "listar";
+    }
+    
+    public List<Concesionario> getCon(){
+        try{
+            return this.ConcesionarioEJB.findAll();
+        }catch (Exception e) {
+            manejarError(e);
+        }
+        return null;
+    }
+     public void elimarConcesioanrio (Concesionario con){
+        try {
+            ConcesionarioEJB.remove(con);
+            manejarExito("Eliminado");
+        }catch (Exception e){
+            manejarError (e);
+        }
+    }
+    public void leer(Concesionario leerConcesionario){
+        concesionario = leerConcesionario;
+        this.setAccion("M");
+    }
+    public void modificar (){
+        
+          try {
+             ConcesionarioEJB.edit(concesionario);
+            manejarExito("Editado");
+        }catch (Exception e){
+            manejarError (e);
+        }
+    }
+    
+    
+    
+    private void manejarExito(String operacion) {
+        String msg = "Se ha realizado exitosamente la operacion de " + operacion;
+        FacesContext context =FacesContext.getCurrentInstance();
+        context.addMessage(null,new FacesMessage(msg));
+        FacesMessage sal = new FacesMessage(FacesMessage.SEVERITY_INFO,"Operación exitosa: ", msg);
+        RequestContext.getCurrentInstance().showMessageInDialog(sal);
+    }
+    private void manejarError(Exception e) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Se produjo el sigiente error: ", e.getMessage()));
+        FacesMessage msg =new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error al insertar: ", e.getMessage());
+        RequestContext.getCurrentInstance().showMessageInDialog(msg);
+    }
+}
